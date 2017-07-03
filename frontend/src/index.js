1fc6d1af1831464829d030996e973519e45956e8
Walter Berggren <walter.berggren@gmail.com>
2017-07-03 20:30:38 +0300

    Set up redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/app/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import Store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = Store();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
